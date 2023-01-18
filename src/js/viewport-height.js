1e9547c600df24f543a528cc2319663435a185df
Jon Winsley <jon.winsley@gmail.com>
2023-01-18 13:57:53 +0000

    running setVH automatically

--- src/js/viewport-height.js
+++ src/js/viewport-height.js
@@ -1,6 +1,10 @@
 // see https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 
-window.addEventListener('resize', () => {
+function setVH() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}
+
+window.addEventListener('resize', setVH());
+
+setVH();
