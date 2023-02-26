a63f738ab3cdc118b43360c20e988a58c9eddc47
Jon Winsley <jon.winsley@gmail.com>
2023-02-26 22:08:33 +0000

    Fixing next chapters

--- utils/bible.js
+++ utils/bible.js
@@ -42,13 +42,26 @@ async function main() {
         books[book].chapters[parsedReference.chapter][parsedReference.from] = cleanText(wrapVerse(verses[reference], parsedReference.from));
     }
 
+    const chapterList = Object.entries(books).map(([book, { chapters }]) => Object.keys(chapters).map(chapter => `/${slugify(book, { lower: true })}/${chapter}/`)).flat();
+    const nextChapter = (slug) => {
+        const index = chapterList.indexOf(slug);
+        if (index === -1) return undefined;
+        return chapterList[index + 1];
+    }
+    const prevChapter = (slug) => {
+        const index = chapterList.indexOf(slug);
+        if (index === -1) return undefined;
+        return chapterList[index - 1];
+    }
+    console.log(chapterList);
+
     const chapters = Object.entries(books).map(([book, { chapters, slug: bookSlug }]) => Object.entries(chapters).map(([chapter, verses]) => ({
         book,
         chapter,
         slug: `${bookSlug}${chapter}/`,
         content: Object.values(verses).join("\n"),
-        nextChapter: chapters[parseInt(chapter) + 1] ? `${bookSlug}${parseInt(chapter) + 1}/` : undefined,
-        prevChapter: chapters[parseInt(chapter) - 1] ? `${bookSlug}${parseInt(chapter) - 1}/` : undefined,
+        nextChapter: nextChapter(`${bookSlug}${chapter}/`),
+        prevChapter: prevChapter(`${bookSlug}${chapter}/`),
     }))).flat();
 
     const output = JSON.stringify({
